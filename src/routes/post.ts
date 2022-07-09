25b948084ffb70640cb81fd59b4eac1ccc5b5bc9
Agreatdayforfreedom <matias.bertoni2002@gmail.com>
2022-07-09 19:28:18 -0300

    post and user, related

--- src/routes/post.ts
+++ src/routes/post.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 import { getPosts, getPost, newPost, updatePost, deletePost } from '../controllers/posts';
+import checkAuth from '../middlewares/checkAuth';
 
 const routerPost = express.Router();
 
 routerPost.get('/', getPosts);
 routerPost.get('/:id', getPost);
-routerPost.post('/new', newPost);
-routerPost.put('/update/:id', updatePost);
-routerPost.delete('/delete/:id', deletePost);
+routerPost.post('/new', checkAuth, newPost);
+routerPost.put('/update/:id', checkAuth, updatePost);
+routerPost.delete('/delete/:id', checkAuth, deletePost);
 
 export default routerPost;
 
