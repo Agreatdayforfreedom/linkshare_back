4e82e798e6defa1e7f128508182ebaef3441f381
Agreatdayforfreedom <matias.bertoni2002@gmail.com>
2022-07-12 23:15:45 -0300

    base comments finished

--- src/controllers/commentPost.ts
+++ src/controllers/commentPost.ts
@@ -2,9 +2,10 @@ import { Request, Response } from "express";
 import CommentPost from "../models/CommentPost";
 
 const getComments = async(request: Request, response: Response) => {
-  const { post } = request.body;    
+  const { id } = request.params;  
+  console.log(request.params)
   try {
-    const comments = await CommentPost.find({post}).populate('emitter');
+    const comments = await CommentPost.find({post: id}).populate('emitter').populate('post');
     response.json(comments) 
   } catch (error) {
     console.log(error);
@@ -12,7 +13,12 @@ const getComments = async(request: Request, response: Response) => {
 }
 const newComment = async(request: Request, response: Response) => {
   const { comment, post } = request.body; 
+  console.log(post)
   try {
+    if(comment === '') {
+      const err: Error = new Error('The comment cannot be empty');
+      return response.status(400).json({ msg: err.message });
+    }
     const newComment = new CommentPost({comment});
     newComment.post = post;
     newComment.emitter = request.user._id;
@@ -23,4 +29,41 @@ const newComment = async(request: Request, response: Response) => {
   }
 }
 
-export { newComment, getComments };
+const updateComment = async(request: Request, response: Response) => {
+  const { id } = request.params;    
+  try {
+    const updateComment = await CommentPost.findById({_id: id}); 
+    if(updateComment){
+      if(updateComment.emitter.toString() !== request.user._id.toString()){
+        const err: Error = new Error('Only the emitter can update this comment');
+        return response.status(401).json({ msg: err.message});
+      } 
+      
+      updateComment.comment = request.body.comment || updateComment.comment;
+      const commentSaved = await updateComment.save();
+      response.json(commentSaved);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+const deleteComment = async(request: Request, response: Response) => {
+  const { id } = request.params;    
+  try {
+    const deleteComment = await CommentPost.findById({_id: id}); 
+    if(deleteComment){
+      if(deleteComment.emitter.toString() !== request.user._id.toString()){
+        const err: Error = new Error('Only the emitter can delete this comment');
+        return response.status(401).json({ msg: err.message});
+      } 
+      
+      await deleteComment.deleteOne();
+      response.json({msg: 'ok'});
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+
+export { newComment, getComments, updateComment, deleteComment };
--- src/routes/commentPost.ts
+++ src/routes/commentPost.ts
@@ -1,11 +1,13 @@
 import express from 'express';
-import { newComment, getComments } from '../controllers/commentPost';
+import { newComment, getComments, updateComment, deleteComment } from '../controllers/commentPost';
 import checkAuth from '../middlewares/checkAuth';
 
 const routerCommentPost = express.Router();
 
-routerCommentPost.get('/', getComments)
-routerCommentPost.post('/new', checkAuth, newComment)
+routerCommentPost.get('/:id', getComments);
+routerCommentPost.post('/new', checkAuth, newComment);
+routerCommentPost.put('/update/:id', checkAuth, updateComment);
+routerCommentPost.delete('/delete/:id', checkAuth, deleteComment);
 
 export default routerCommentPost;
 
